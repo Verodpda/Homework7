5ac6150539c7d71d0c1bc1903cbba83bd5abb5a8
Exadat <jdiaza93@gmail.com>
2022-12-29 19:51:20 -0300

    Added weather icon change

--- style/index.js
+++ style/index.js
@@ -1,3 +1,4 @@
+//time and date
 let currentDate = new Date();
 let h2 = document.querySelector("h2");
 let daysWeek =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -10,7 +11,7 @@ h2.innerHTML = `${day} ${hour}:${minutes}`;
 h2.innerHTML = `${day} ${hour}:0${minutes}`;
 }
 
-
+//current position
   function currentPosition (position){
       function displayCityWeather (response){
       let currentTemp = document.querySelector("#current-temp");
@@ -28,8 +29,41 @@ h2.innerHTML = `${day} ${hour}:0${minutes}`;
       let cityName = document.querySelector("h1");
       cityName.innerHTML = response.data.name;
 
+      let weatherIconURL = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+      let weatherIcon = document.querySelector("#weather-image");
+      weatherIcon.setAttribute("src", `images/cloudy.svg`);
+
   }
    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=70a117d10548a7cde81c5d73ab55d01b&units=metric`;
    axios.get(apiUrl).then(displayCityWeather)
   }
   navigator.geolocation.getCurrentPosition(currentPosition);
+
+
+//city search
+  function citySubmit(event){
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let cityHTML = document.querySelector("h1");
+  cityHTML.innerHTML = (`${cityInput.value}`)
+
+  function displayCityWeather (response){
+      let currentTemp = document.querySelector("#current-temp");
+      currentTemp.innerHTML = `${Math.round(response.data.main.temp_max)}/${Math.round(response.data.main.temp_min)} C°|F°`;
+
+      let currentHumidity = document.querySelector("#humidity");
+      currentHumidity.innerHTML = `${response.data.main.humidity}%`;
+
+      let currentWindSpeed = document.querySelector("#wind");
+      currentWindSpeed.innerHTML = `${Math.round(response.data.wind.speed)} Km/h`
+
+      let weatherDescription = document.querySelector("#weather-description");
+      weatherDescription.innerHTML = (response.data.weather[0].description);
+
+  }
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=70a117d10548a7cde81c5d73ab55d01b&units=metric`;
+  axios.get(apiUrl).then(displayCityWeather)
+}
+
+let cityForm = document.querySelector("#city-search");
+cityForm.addEventListener("submit", citySubmit);
