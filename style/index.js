f0d96764942d2fe7b0c97a3b938ad635940114bb
Exadat <jdiaza93@gmail.com>
2022-12-29 17:04:38 -0300

    Added current city, weather, humidity and wind

--- style/index.js
+++ style/index.js
@@ -9,3 +9,27 @@ h2.innerHTML = `${day} ${hour}:${minutes}`;
 } else {
 h2.innerHTML = `${day} ${hour}:0${minutes}`;
 }
+
+
+  function currentPosition (position){
+      function displayCityWeather (response){
+      let currentTemp = document.querySelector("#current-temp");
+      currentTemp.innerHTML = `${Math.round(response.data.main.temp_max)}/${Math.round(response.data.main.temp_min)} C°|F°`;
+
+      let currentHumidity = document.querySelector("#humidity");
+      currentHumidity.innerHTML = `${response.data.main.humidity}%`;
+
+      let currentWindSpeed = document.querySelector("#wind");
+      currentWindSpeed.innerHTML = `${Math.round(response.data.wind.speed)} Km/h`
+
+      let weatherDescription = document.querySelector("#weather-description");
+      weatherDescription.innerHTML = (response.data.weather[0].description);
+
+      let cityName = document.querySelector("h1");
+      cityName.innerHTML = response.data.name;
+
+  }
+   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=70a117d10548a7cde81c5d73ab55d01b&units=metric`;
+   axios.get(apiUrl).then(displayCityWeather)
+  }
+  navigator.geolocation.getCurrentPosition(currentPosition);
